fa23cb369d4b4d2271204a68a21d0e4646507677
Oscar Mulder <omulder@student.codam.nl>
2019-02-07 21:03:44 +0100

    Still broken but okay

--- print_var.c
+++ print_var.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/02/07 13:52:31 by omulder        #+#    #+#                */
-/*   Updated: 2019/02/07 18:13:35 by omulder       ########   odam.nl         */
+/*   Updated: 2019/02/07 21:01:27 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,11 +27,23 @@ int		print_padding(char c, int size)
 
 int		printed_chars(t_fmt *fmt, int num)
 {
-	if (fmt->prec > fmt->width && num >= 0)
-		return (fmt->prec);
-	if (fmt->prec >= fmt->width && num < 0)
-		return (fmt->prec + 1);
-	return (fmt->width);
+	int	ilen;
+
+	ilen = ft_intlen(num);
+	if (num < 0)
+		ilen++;
+	if (fmt->prec > ilen || fmt->width > ilen)
+	{
+		if (fmt->prec > fmt->width && num >= 0)
+			ilen = fmt->prec;
+		else if (fmt->prec >= fmt->width && num < 0)
+			ilen = fmt->prec + 1;
+		else
+			ilen = fmt->width;
+	}
+	if (num >= 0 && fmt->opt[3])
+		ilen++;
+	return (ilen);
 }
 
 int		print_int(t_fmt *fmt, int num)
@@ -41,18 +53,29 @@ int		print_int(t_fmt *fmt, int num)
 	ilen = ft_intlen(num);
 	if (fmt->prec > ilen)
 		ilen = fmt->prec;
-	if (num < 0)
+	if (num < 0 || (num >= 0 && fmt->opt[3]) || fmt->opt[4])
 	ilen++;
 	if (num < 0)
 	{
 		if (fmt->prec == -1 && fmt->opt[1])
 			ft_putchar('-');
-		if (!fmt->opt[2] && fmt->opt[1])
+		if (!fmt->opt[2] && (!fmt->opt[1] || fmt->prec != -1))
 			print_padding(' ', (fmt->width - ilen));
-		if (!fmt->opt[2] && fmt->opt[1])
+		if (!fmt->opt[2] && fmt->prec == -1)
 			print_padding('0', (fmt->width - ilen));
-		if (!fmt->opt[1])
+		if (!fmt->opt[1] || fmt->prec != -1)
 			ft_putchar('-');
+	}
+	else
+	{
+		if ((!fmt->opt[2] && !fmt->opt[1]) && (!fmt->opt[2] && fmt->opt[4]))
+			print_padding(' ', (fmt->width - ilen));
+		if (fmt->opt[3] && !fmt->opt[4])
+			ft_putchar(' ');
+		if (fmt->opt[4])
+			ft_putchar('+');
+		if (!fmt->opt[2] && fmt->prec == -1)
+			print_padding('0', (fmt->width - ilen));
 	}
 	if (fmt->prec != -1)
 		print_padding('0', (fmt->prec - ft_intlen(num)));

9388749833e4fb8ec16d6d8011559c53812d8523
Oscar Mulder <omulder@student.codam.nl>
2019-02-28 20:02:28 +0100

    Fix pointer return

--- printed_uchars.c
+++ printed_uchars.c
@@ -6,7 +6,7 @@
 /*   By: omulder <omulder@student.codam.nl>           +#+                     */
 /*                                                   +#+                      */
 /*   Created: 2019/02/12 16:00:06 by omulder        #+#    #+#                */
-/*   Updated: 2019/02/28 19:43:25 by omulder       ########   odam.nl         */
+/*   Updated: 2019/02/28 19:58:15 by omulder       ########   odam.nl         */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		printed_uchars(t_fmt fmt, unsigned long long num)
 		rilen = 0;
 	if (fmt.prec == 0 && num == 0 && fmt.conv == 'p')
 		rilen = 2;
-	if (fmt.prec > ilen || fmt.width > ilen)
+	if (fmt.prec > ilen || fmt.width > rilen)
 	{
 		if (fmt.prec > fmt.width)
 		{
